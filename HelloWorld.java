5ae7bdbba1f484c93a47fa69cb5c502463aab5a9
Li Cao <li.cao@sap.com>
2016-04-01 15:30:46 +0800

    initial

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sap.sakp.lili.lab;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		saySomething("hello, world!");
+
+	}
+	
+	public static void saySomething(String say) {
+		for (int i=0; i<3; i++) {
+			System.out.println(say);
+		}
+	}
+
+}
